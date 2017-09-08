f925d908c748eeadf9aaecba92c56b6dfed5b27b
yuga528 <31730580+yuga528@users.noreply.github.com>
2017-09-08 16:12:16 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -6,5 +6,6 @@ class Student{
   Student s1=new Student();//creating an object of Student  
   System.out.println(s1.id);//accessing member through reference variable  
   System.out.println(s1.name);  
+  System.out.println("royal");
  }  
 }
